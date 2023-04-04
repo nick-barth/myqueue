a7675335215e792f1e886da1032223b6673987c8
nick-barth <barth.nicholas@gmail.com>
2023-04-04 22:17:18 +0200

    quit your bitchin typescript

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,45 @@ import { SupabaseClient, Session } from '@supabase/supabase-js';
 import { Database } from './DatabaseDefinitions';
 
 declare global {
+	declare module '*.svg?component' {
+		import type { ComponentType, SvelteComponentTyped } from 'svelte';
+		import type { SVGAttributes } from 'svelte/elements';
+
+		const content: ComponentType<SvelteComponentTyped<SVGAttributes<SVGSVGElement>>>;
+
+		export default content;
+	}
+
+	declare module '*.svg?src' {
+		const content: string;
+		export default content;
+	}
+
+	declare module '*.svg?url' {
+		const content: string;
+		export default content;
+	}
+
+	declare module '*.svg?dataurl' {
+		const content: string;
+		export default content;
+	}
+
+	declare module '*.svg?dataurl=base64' {
+		const content: string;
+		export default content;
+	}
+
+	declare module '*.svg?dataurl=enc' {
+		const content: string;
+		export default content;
+	}
+
+	declare module '*.svg?dataurl=unenc' {
+		const content: string;
+		export default content;
+	}
+
 	namespace App {
 		interface Locals {
 			supabase: SupabaseClient<Database>;
--- src/lib/components/player.svelte
+++ src/lib/components/player.svelte
@@ -2,6 +2,8 @@
 	import { combineMeta } from '$lib/utils/bookmark';
 	import { onMount } from 'svelte';
 	import { intervalToDuration } from 'date-fns';
+	import PlayButton from '$lib/icons/play-button.svg?component';
+	import CloseButton from '$lib/icons/close-button.svg?component';
 
 	import type { BookmarkType } from '$types/types';
 
@@ -40,35 +42,8 @@
 <aside>
 	<div>
 		<img class="min-w-full max-h-56" src={bookmark.image} alt="Related to the article" />
-		<button class="absolute top-6 right-6" on:click={() => setSelectedBookmark(null)}
-			><svg
-				width="34"
-				height="34"
-				viewBox="0 0 34 34"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					opacity="0.24"
-					d="M1 16.9974C1.08668 17.9661 1.12863 18.9407 1.26285 19.8974C1.87243 24.1919 3.76829 27.6947 7.23005 29.9889C12.7554 33.6567 18.6192 33.8276 24.5556 31.2935C27.5616 30.0099 29.8741 27.7487 31.3589 24.6088C32.95 21.2409 33.3191 17.6812 32.7459 13.9834C31.8762 8.39035 28.9346 4.57266 24.0831 2.43139C20.7443 0.958896 17.2602 0.700985 13.7285 1.30078C10.6387 1.8256 7.81726 3.05818 5.48798 5.37638C2.3422 8.51031 1.05872 12.4779 1 16.9974Z"
-					stroke="white"
-					stroke-width="2"
-				/>
-				<path
-					d="M13.2354 13.0006L20.7648 21.001"
-					stroke="white"
-					stroke-width="2"
-					stroke-linecap="square"
-					stroke-linejoin="round"
-				/>
-				<path
-					d="M20.7648 13.0006L13.2354 21.001"
-					stroke="white"
-					stroke-width="2"
-					stroke-linecap="square"
-					stroke-linejoin="round"
-				/>
-			</svg>
+		<button class="absolute top-6 right-6" on:click={() => setSelectedBookmark(null)}>
+			<CloseButton />
 		</button>
 	</div>
 	<div class="p-6">
@@ -95,16 +70,7 @@
 		</div>
 		<div class="w-full mt-6 flex items-center justify-center">
 			<button class="bg-colors-primary rounded-full h-16 w-16 flex items-center justify-center"
-				><svg
-					class="pl-1"
-					width="20"
-					height="23"
-					viewBox="0 0 20 23"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path d="M0.046511 22.1935L0 0.557571L20 11.0073L0.046511 22.1935Z" fill="#F4F4F4" />
-				</svg>
+				><PlayButton />
 			</button>
 		</div>
 		<figure>
