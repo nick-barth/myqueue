d747757abda518c98f7136231d96e28ccb13bb63
nick-barth <barth.nicholas@gmail.com>
2023-05-05 09:44:17 +0200

    some fixes

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -41,31 +41,30 @@
 	];
 </script>
 
-<div class="w-full h-[100vh] sticky top-0">
+<div class="w-full h-[100vh] sticky top-0 border-r border-gray900 py-10">
 	<nav class="flex flex-col h-full">
-		<div class="flex border-b border-gray900 mb-2">
+		<div class="flex border-b border-gray900">
 			<a
 				href="/"
-				class="flex flex-row items-center gap-4 px-4 py-4 pb-8 mt-2 w-full hover:bg-gray950 text-domine font-bold"
+				class="flex pl-10 flex-row items-center gap-4 py-4 pb-8 w-full hover:bg-gray950 text-domine font-bold"
 			>
 				<div class="h-8 w-8">
 					<LogoIcon />
 				</div>
 				<div class="pt-2">My Queue</div>
 			</a>
-			<p class="text-lg px-4 mb-2 md:hidden">Add articles to your queue</p>
+			<p class="text-lg mb-2 md:hidden">Add articles to your queue</p>
 		</div>
 		{#each sources as source}
 			<a
 				href={`/discover/${source.source}`}
-				class=" list-none py-3 w-full hover:bg-gray950 px-4 flex items-center gap-4
-                    {$page.params.source === source.source && 'bg-gray950'}"
+				class=" pl-10 list-none py-3 w-full hover:bg-gray950 flex items-center gap-4"
 			>
 				<img class="h-8 w-8" src="/images/sources/{source.source}.png" alt={source.title} />
 				{source.title}
 			</a>
 		{/each}
-		<div class="flex h-full p-2 flex-col justify-end">
+		<div class=" pl-10 flex h-full p-2 flex-col justify-end">
 			<div>
 				<ContextMenu position="left" positionY="top">
 					<div slot="icon" class="overflow-hidden rounded-full h-10 w-10">
@@ -88,16 +87,14 @@
 						<a
 							href="https://www.myqueue.so/support"
 							target="_blank"
-							class="hover:bg-background px-4 py-2 flex align-center items-center"
+							class="hover:bg-background py-2 flex align-center items-center"
 						>
 							Support
 							<div class=" h-4 w-4">
 								<ArrowDiag />
 							</div>
 						</a>
-						<button on:click={handleLogout} class="hover:bg-background w-full px-4 py-2"
-							>Logout</button
-						>
+						<button on:click={handleLogout} class="hover:bg-background w-full py-2">Logout</button>
 					</div>
 				</ContextMenu>
 			</div>
