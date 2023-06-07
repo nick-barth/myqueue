d1ce325b10b60f0e538e7deb520ba902c632b7f0
Nick <barth.nicholas@gmail.com>
2023-06-07 17:28:30 +0200

    did it up

--- src/lib/components/discovery-bar.svelte
+++ src/lib/components/discovery-bar.svelte
@@ -1,8 +1,6 @@
 <script>
 	import { sources } from '$lib/constants/sources';
 	import { page } from '$app/stores';
-
-	console.log($page);
 </script>
 
 <aside class="flex w-full overflow-scroll h-full items-center max-w-2xl m-auto md:justify-center">
@@ -15,14 +13,10 @@
 		/>
 		My Queue
 	</a>
-	<a
-		href={`/add`}
-		class="list-none px-2 flex flex-col text-sm items-center flex-shrink-0
-{$page.url.pathname === `/add` && 'bg-gray950'}"
-	>
+	<a href={`/add`} class="list-none px-2 flex flex-col text-sm items-center flex-shrink-0">
 		<img
 			class="h-[60px] w-[60px] rounded-full flex-shrink-0 border-4 border-solid border-white {$page
-				.url.pathname === 'add' && 'shadow-[0_0px_0px_2px_black]'}"
+				.url.pathname === '/add' && 'shadow-[0_0px_0px_2px_black]'}"
 			src="/images/sources/add-url.png"
 			alt="add url"
 		/>
