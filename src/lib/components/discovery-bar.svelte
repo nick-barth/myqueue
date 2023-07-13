04f37f0feea1d4f2336cd356a2ebbcb3426fb358
Nick <barth.nicholas@gmail.com>
2023-07-13 20:59:14 +0200

    made icons bigger

--- src/lib/components/discovery-bar.svelte
+++ src/lib/components/discovery-bar.svelte
@@ -25,7 +25,7 @@
 	</a>
 	<a href={`/add`} class="list-none px-1.5 flex flex-col text-sm items-center flex-shrink-0">
 		<img
-			class="h-[60px] w-[60px] rounded-full flex-shrink-0 border-4 border-solid mb-0.5 border-white {$page
+			class="h-[60px] w-[60px] rounded-full flex-shrink-0 border-solid mb-0.5 border-white {$page
 				.url.pathname === '/add' && 'shadow-[0_0px_0px_2px_#c8c8c8]'}"
 			src="/images/sources/add-url.png"
 			alt="add url"
@@ -38,8 +38,8 @@
 			class="list-none px-1.5 flex flex-col text-sm items-center flex-shrink-0"
 		>
 			<img
-				class="h-[60px] w-[60px] rounded-full flex-shrink-0 border-4 border-solid mb-0.5 border-white {$page
-					.params.source === source.source && 'shadow-[0_0px_0px_2px_#c8c8c8]'}"
+				class="h-[60px] w-[60px] rounded-full flex-shrink-0 mb-0.5 border-white {$page.params
+					.source === source.source && 'shadow-[0_0px_0px_2px_#c8c8c8]'}"
 				src="/images/sources/{source.source}.png"
 				alt={source.title}
 			/>
