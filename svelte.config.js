22177171f61370dd9feba31944b9126c2563f85a
Nick <barth.nicholas@gmail.com>
2023-07-14 21:32:28 +0200

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const sources = {
@@ -16,13 +17,14 @@ const routes = ['/signup', '/', '/add', '/read', '/signin', '/signin', ...discov
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-		}),
+		adapter: adapter(),
+		// adapter: adapter({
+		// 	// default options are shown. On some platforms
+		// 	// these options are set automatically — see below
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: 'index.html',
+		// }),
 		prerender: {
 			entries: routes
 		},
